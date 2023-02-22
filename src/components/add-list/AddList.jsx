5966140fbd5b496efee9ea79eabba0aceb7f1096
DavronRaimjonov <davron_raimjonov4446@mail.ru>
2023-02-22 22:24:56 +0500

    server json

--- src/components/add-list/AddList.jsx
+++ src/components/add-list/AddList.jsx
@@ -1,26 +1,49 @@
-import React, { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./AddList.scss";
 import Listitem from "../list-item/Listitem";
-import { getForm, getValue } from "../../servic";
+import { request } from "../config/axios";
 const AddList = () => {
   const [value, setValue] = useState({ plan: "" });
   const [value2, setValue2] = useState([]);
   const ref = useRef();
-  const delteList = (id) => {
+  const getValue = (e) => {
+    setValue((value) => ({ ...value, [e.target.name]: e.target.value }));
+  };
+
+  const getForm = (e) => {
+    e.preventDefault();
+    if (value.plan === "") {
+      ref.current.style.border = "1px solid red";
+      return;
+    } else {
+      ref.current.style.border = "none";
+    }
+    request.post("/lists", value);
+    setValue({ plan: "" });
+  };
+  const delteList = async (id) => {
+    await request.delete("/lists/" + id);
     setValue2((p) => p.filter((item) => item.id != id));
   };
-  const edit = (obj) => {
-    setValue2((p) => p.map((item) => (item.id == obj.id ? obj : item)));
+  const edit = async (obj) => {
+    let response = await request.put("/lists/" + obj.id, obj);
+    const { id } = response.data;
+    setValue2((p) =>
+      p.map((item) => (item.id == id ? { ...response.data } : item))
+    );
   };
+  useEffect(() => {
+    request.get(`/lists`).then((data) => setValue2(data.data));
+  }, [value]);
   return (
     <div className="add_list">
-      <form onSubmit={(e) => getForm(e, setValue, setValue2, value, ref)}>
+      <form onSubmit={getForm}>
         <input
           maxLength={55}
           ref={ref}
-          value={value.plan}
-          onChange={(e) => getValue(e, setValue)}
           name="plan"
+          onChange={getValue}
+          value={value.plan}
           type="text"
           placeholder="What do you have planed ?"
         />
